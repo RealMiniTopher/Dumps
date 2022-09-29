32abfa8edc0753a2fe7e7bdce292d1e6dc337a32
Christian Mora <114608984+RealMiniTopher@users.noreply.github.com>
2022-09-29 10:50:03 -0400

    Add files via upload

---
+++ Random/Random4.py
@@ -0,0 +1,24 @@
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./100)
+
+sprint('Lacye Rocks! ' * 99999)
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./100)
+
+sprint('Lacye Rocks! ' * 99999)
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./100)
+
+sprint('Lacye Rocks! ' * 99999)
