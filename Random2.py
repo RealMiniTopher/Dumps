da8f811517f96f3371fdbe61d02e854d26b6ab29
Christian Mora <114608984+RealMiniTopher@users.noreply.github.com>
2022-09-29 08:27:39 -0400

    Add files via upload

---
+++ Random2.py
@@ -0,0 +1,184 @@
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('If you are seeing this you have made a big mistake DONT LEAVE JUST FOLLOW MY INSTRUCTIONS')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('FIRST OPEN A NEW TAB AND DO LEAVE IT BLANK')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('THEN TURN AROUND AND SAY SUSSY BAKA')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('THEN POST KANYE 2024 RALLY AT MY PLACE ON TWITTER')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('AND THEN POST YOUR ADDRESS')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('AND THEN FOLLOW IT UP WITH A BUNCH OF POLITICAL STATEMENTS')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('AFTER THAT GO ON REDDIT AND POST A BUNCH OF POLITICAL STUFF')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('THEN MAKE THE US ECONOMY COLLAPSE')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('THEN TWEET OUT THAT YOU ARE AGAINST PEOPLE WHO ARE ATTRACTED TO POPTARTS')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('THEN TELL YOUR PARENTS THAT YOU ARE CHANGING YOUR LEGAL NAME')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('THEN CHANGE YOUR LEGAL NAME TO SOMETHING THAT IS STRANGE')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('THEN INSTALL A VIRUS ON YOUR DEVICE')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('THEN GO TO ADFLY AND CLICK ON EVERY LINK THAT YOU SEE')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('THEN TRY TO DO A CHALLENGE WHERE WHOEVER CAN GET MORE DEBT WINS!')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('AFTER THAT WHY NOT BINGE WATCH DORA THE EXPLORER')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('ALSO PLEASE SHARE THIS CODE WITH EVERYBODY YOU KNOW')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('UR MOMMA SO FAT')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('THEN GO TO VIRUS.COM I HAVENT SEEN WHAT IS ON THAT WEBSITE AND SO I WANT YOU TO TEST IT OUT')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('WITH A PISTOL ON MY HIP LIKE IMMA COP')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint("GUCCI GANG GUCCI GANG")
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('KELDON JOHNSON IS THE GOAT')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('GO SPURS GO')
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./8)
+
+sprint('')
