cea4f1857a1f869190c799339987e8ccbf18262f
Christian Mora <114608984+RealMiniTopher@users.noreply.github.com>
2022-09-29 14:51:18 -0400

    Add files via upload

---
+++ Random/Random5.py
@@ -0,0 +1,80 @@
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./50)
+
+sprint('😳 ' * 999999)
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./50)
+
+sprint('😳 ' * 999999)
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./50)
+
+sprint('😳 ' * 999999)
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./50)
+
+sprint('😳 ' * 999999)
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./50)
+
+sprint('😳 ' * 999999)
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./50)
+
+sprint('😳 ' * 999999)
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./50)
+
+sprint('😳 ' * 999999)
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./50)
+
+sprint('😳 ' * 999999)
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./50)
+
+sprint('😳 ' * 999999)
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./50)
+
+sprint('😳 ' * 999999)
